8d1808c1e17a0cf217f3ad20c051680e73cf650a
Carles Capellas <capellas.carles@gmail.com>
2020-11-22 16:48:58 +0100

    feat: generate static pages for blog articles

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,15 +1,19 @@
 const merge = require('webpack-merge');
 const { resolve } = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const { ArticleId } = require('../src/components/articles/article-id');
 const baseConfig = require('./base.config');
+
+const routes = ['/', '/blog', '/portfolio'].concat(
+    Object.values(ArticleId).map((articleId) => `/blog/${articleId}`)
+);
 
 module.exports = merge(baseConfig, {
     mode: 'production',
     plugins: [
         new PrerenderSPAPlugin({
             staticDir: resolve(__dirname, '..', 'docs'),
-            // TODO Compute articles url to be prerendered
-            routes: ['/', '/blog', '/portfolio']
+            routes
         })
     ]
 });
