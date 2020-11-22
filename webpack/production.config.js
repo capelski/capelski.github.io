cb349e6c6683b0b49587cd14946f288b272ba4a4
Carles Capellas <capellas.carles@gmail.com>
2020-11-22 10:30:49 +0100

    feat: generate static assets for some urls

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,6 +1,15 @@
 const merge = require('webpack-merge');
+const { resolve } = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const baseConfig = require('./base.config');
 
 module.exports = merge(baseConfig, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new PrerenderSPAPlugin({
+            staticDir: resolve(__dirname, '..', 'docs'),
+            // TODO Compute articles url to be prerendered
+            routes: ['/', '/blog', '/portfolio']
+        })
+    ]
 });
